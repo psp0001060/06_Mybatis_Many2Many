875fcef2cb29700ab03761da2fc0fa6601e8e20d
psp0001060 <psp0001060@613.com>
2018-04-20 16:43:04 +0800

    db sql

---
+++ src/db.sql
@@ -0,0 +1,72 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.0.22-community-nt : Database - mybatsis_many2many
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mybatsis_many2many` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `mybatsis_many2many`;
+
+/*Table structure for table `course` */
+
+DROP TABLE IF EXISTS `course`;
+
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL,
+  `course_code` varchar(10) NOT NULL,
+  `course_name` varchar(10) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `course` */
+
+insert  into `course`(`id`,`course_code`,`course_name`) values (1,'A01','JAVA'),(2,'A02','Mysql');
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` tinytext,
+  `sex` tinytext,
+  `age` int(4) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+insert  into `student`(`id`,`name`,`sex`,`age`) values (1,'Tom\r\n','man',23),(2,'Mary','women',22),(3,'zs','man',23);
+
+/*Table structure for table `student_course` */
+
+DROP TABLE IF EXISTS `student_course`;
+
+CREATE TABLE `student_course` (
+  `id` decimal(3,0) NOT NULL,
+  `student_id` int(11) default NULL,
+  `course_id` int(3) default NULL,
+  PRIMARY KEY  (`id`),
+  KEY `stuFK` (`student_id`),
+  KEY `courseFK` (`course_id`),
+  CONSTRAINT `courseFK` FOREIGN KEY (`course_id`) REFERENCES `course` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `stuFK` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student_course` */
+
+insert  into `student_course`(`id`,`student_id`,`course_id`) values (1,1,2),(2,1,1),(3,2,1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
